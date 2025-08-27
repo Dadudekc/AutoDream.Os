69d85a49fe0ffeee3ab873d5e526e94a3d8bc114
Victor Dixon <135445391+Dadudekc@users.noreply.github.com>
2025-08-27 07:21:37 -0500

    centralize package init template

---
+++ src/task_distribution/agents/__init__.py
@@ -0,0 +1,3 @@
+"""Task distribution agents package initializer using canonical template."""
+
+from src.package_template import *  # noqa: F401,F403
---
+++ src/web/frontend/testing/__init__.py
@@ -0,0 +1,3 @@
+"""Web frontend testing package initializer using canonical template."""
+
+from src.package_template import *  # noqa: F401,F403

b5fd4f902230de719724972ca9d396c4c3a932ec
Victor Dixon <135445391+Dadudekc@users.noreply.github.com>
2025-08-29 07:13:33 -0500

    chore: remove obsolete backup directories

--- ai_ml_backup_20250828_221414/managers/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Manager modules for model lifecycle, training, and evaluation."""
-
-from .model_manager import ModelManager
-from .training_manager import TrainingManager
-from .evaluation_manager import EvaluationManager
-
-__all__ = ["ModelManager", "TrainingManager", "EvaluationManager"]

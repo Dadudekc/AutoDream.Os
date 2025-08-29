0980ebe66ac3a0848920c213506f36b569c7a84b
Victor Dixon <135445391+Dadudekc@users.noreply.github.com>
2025-08-29 08:28:52 -0500

    Refactor FSM compliance modules

--- src/fsm/__init__.py
+++ src/fsm/__init__.py
@@ -9,7 +9,7 @@ __status__ = "MODULARIZED"
 
 # Core imports
 from .core.engine.execution_engine import ExecutionEngine
-from .core.states.state_manager import StateManager
+from .state_manager import StateManager
 from .core.transitions.transition_manager import TransitionManager
 from .core.workflows.workflow_manager import WorkflowManager
 
@@ -23,38 +23,39 @@ from .utils.config import FSMConfig
 
 __all__ = [
     "ExecutionEngine",
-    "StateManager", 
+    "StateManager",
     "TransitionManager",
     "WorkflowManager",
     "IStateManager",
     "ITransitionManager",
     "IWorkflowManager",
-    "FSMConfig"
+    "FSMConfig",
 ]
+
 
 # Main FSM Core V2 class
 class FSMCoreV2:
     """Main entry point for modular FSM Core V2"""
-    
+
     def __init__(self):
         self.state_manager = StateManager()
         self.transition_manager = TransitionManager()
         self.workflow_manager = WorkflowManager()
         self.execution_engine = ExecutionEngine()
         self.config = FSMConfig()
-    
+
     def initialize(self) -> bool:
         """Initialize FSM Core V2"""
         return True
-    
+
     def get_state_manager(self) -> StateManager:
         """Get state manager instance"""
         return self.state_manager
-    
+
     def get_transition_manager(self) -> TransitionManager:
         """Get transition manager instance"""
         return self.transition_manager
-    
+
     def get_workflow_manager(self) -> WorkflowManager:
         """Get workflow manager instance"""
         return self.workflow_manager
